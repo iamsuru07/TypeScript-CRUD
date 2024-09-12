3e52e85127a4b10c04405e1d2b48781a60ea59f6
Suryansh Shrivastava <suryansh.shrivastava@respo.co.in>
2024-09-12 18:40:38 +0530

    For PR Review

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from 'fastify';
-import userRoutes from './userRoutes';
-import thoughtUploaderRoutes from './thoughtUploaderRoutes';
-
-const routes = async (fastify: FastifyInstance) => {
-    await userRoutes(fastify);
-    await thoughtUploaderRoutes(fastify);
-};
-export default routes;
